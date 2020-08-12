30e080464faa89302e2d770a84c2a6b5626d6293
Anish Ramasekar <anish.ramasekar@gmail.com>
2020-08-12 09:27:21 -0700

    update kv endpoint suffix for custom cloud

--- pkg/azure/provider_test.go
+++ pkg/azure/provider_test.go
@@ -12,41 +12,6 @@ import (
 	"github.com/Azure/go-autorest/autorest/azure"
 )
 
-func TestGetVaultDNSSuffix(t *testing.T) {
-	cases := []struct {
-		cloudName string
-		expected  *string
-	}{
-		{
-			cloudName: "",
-			expected:  &azure.PublicCloud.KeyVaultDNSSuffix,
-		},
-		{
-			cloudName: "AZURECHINACLOUD",
-			expected:  &azure.ChinaCloud.KeyVaultDNSSuffix,
-		},
-		{
-			cloudName: "AZUREGERMANCLOUD",
-			expected:  &azure.GermanCloud.KeyVaultDNSSuffix,
-		},
-		{
-			cloudName: "AZUREPUBLICCLOUD",
-			expected:  &azure.PublicCloud.KeyVaultDNSSuffix,
-		},
-		{
-			cloudName: "AZUREUSGOVERNMENTCLOUD",
-			expected:  &azure.USGovernmentCloud.KeyVaultDNSSuffix,
-		},
-	}
-
-	for _, tc := range cases {
-		actual, _ := GetVaultDNSSuffix(tc.cloudName)
-		if !strings.EqualFold(*tc.expected, *actual) {
-			t.Fatalf("expected: %v, got: %v", *tc.expected, *actual)
-		}
-	}
-}
-
 func TestGetVaultURL(t *testing.T) {
 	testEnvs := []string{"", "AZUREPUBLICCLOUD", "AZURECHINACLOUD", "AZUREGERMANCLOUD", "AZUREUSGOVERNMENTCLOUD"}
 	vaultDNSSuffix := []string{"vault.azure.net", "vault.azure.net", "vault.azure.cn", "vault.microsoftazure.de", "vault.usgovcloudapi.net"}
