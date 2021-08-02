fc1c4bcd8b1fe2114552d617de4b4e3f2f32c55a
Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>
2021-08-02 11:47:46 -0700

    test: updates DS image.
    Signed-off-by: Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>

--- test/e2e/framework/deploy/deploy.go
+++ test/e2e/framework/deploy/deploy.go
@@ -7,11 +7,13 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/Azure/secrets-store-csi-driver-provider-azure/test/e2e/framework"
 	"github.com/Azure/secrets-store-csi-driver-provider-azure/test/e2e/framework/exec"
-	"gopkg.in/yaml.v2"
 	appsv1 "k8s.io/api/apps/v1"
+
+	"sigs.k8s.io/yaml"
 
 	. "github.com/onsi/gomega"
 )
@@ -56,17 +58,27 @@ func InstallManifest(kubeconfigPath string, config *framework.Config) {
 
 		fileBytes, err := ioutil.ReadAll(file)
 		Expect(err).To(BeNil())
-		fmt.Fprintf(GinkgoWriter, "%s", fileBytes)
+
+		fileContent := fmt.Sprintf("%s", fileBytes)
+		pos := strings.LastIndex(fileContent, "---")
+		if pos == -1 {
+			return
+		}
+		adjustedPos := pos + len("---")
+		if adjustedPos >= len(fileContent) {
+			return
+		}
+		dsYaml := fileContent[adjustedPos:len(fileContent)]
 
 		ds := &appsv1.DaemonSet{}
-		err = yaml.Unmarshal(fileBytes, ds)
+		err = yaml.Unmarshal([]byte(dsYaml), ds)
 		Expect(err).To(BeNil())
 
-		ds.Spec.Template.Spec.Containers[0].Image = fmt.Sprintf("%s/%s:%s", config.Registry, config.ImageName, config.ImageVersion)
+		ds.Spec.Template.Spec.Containers[0].Image = fmt.Sprintf("%s/%s:%s", config.Registry, config.ImageName, config.ImageVersion)	
 		updatedDS, err := yaml.Marshal(ds)
 		Expect(err).To(BeNil())
 
-		err = ioutil.WriteFile(fmt.Sprintf("%s/updated-%s", providerResourceAbsolutePath, resource), updatedDS, 0444)
+		err = ioutil.WriteFile(fmt.Sprintf("%s/updated-%s", providerResourceAbsolutePath, resource), updatedDS, 0644)
 		Expect(err).To(BeNil())
 
 		err = exec.KubectlApply(kubeconfigPath, framework.NamespaceKubeSystem, []string{"-f", fmt.Sprintf("%s/%s", providerResourceAbsolutePath, resource)})
