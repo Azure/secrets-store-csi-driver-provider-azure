5628e7dfdae874c3d3e10d38323b1751a0b05d97
Anish Ramasekar <anish.ramasekar@gmail.com>
2021-09-27 15:53:03 -0700

    release: update manifest and helm charts for v1.0.0-rc.0 (#659)
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- test/e2e/framework/config.go
+++ test/e2e/framework/config.go
@@ -1,3 +1,4 @@
+//go:build e2e
 // +build e2e
 
 package framework
@@ -15,7 +16,7 @@ type Config struct {
 	KeyvaultName                      string `envconfig:"KEYVAULT_NAME"`
 	Registry                          string `envconfig:"REGISTRY" default:"mcr.microsoft.com/oss/azure/secrets-store"`
 	ImageName                         string `envconfig:"IMAGE_NAME" default:"provider-azure"`
-	ImageVersion                      string `envconfig:"IMAGE_VERSION" default:"v0.2.0"`
+	ImageVersion                      string `envconfig:"IMAGE_VERSION" default:"v1.0.0-rc.0"`
 	IsSoakTest                        bool   `envconfig:"IS_SOAK_TEST" default:"false"`
 	IsWindowsTest                     bool   `envconfig:"TEST_WINDOWS" default:"false"`
 	IsGPUTest                         bool   `envconfig:"TEST_GPU" default:"false"`
