1e1bfd2310dc42397dd04f34d96c539999c53823
Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>
2022-03-02 18:20:20 +0530

    test: adds log
    Signed-off-by: Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>

--- arc/monitoring/metrics/push.go
+++ arc/monitoring/metrics/push.go
@@ -31,15 +31,19 @@ func PushMetricsToGeneva(w http.ResponseWriter, r *http.Request) {
 
 	decodedRequest, err := snappy.Decode(nil, requestBody)
 	if err != nil {
+		fmt.Printf("Error decoding request: %s", err.Error())
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	writeRequest := prompb.WriteRequest{}
 	if err := proto.Unmarshal(decodedRequest, &writeRequest); err != nil {
+		fmt.Printf("Error unmarshaling request: %s", err.Error())
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	fmt.Printf("Received %d timeseries...", len(writeRequest.Timeseries))
+	fmt.Println("Pushed metrics complete")
 	w.WriteHeader(http.StatusAccepted)
 }
