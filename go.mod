f619ba366b7840997c9bed5c44a89cd727be79ef
Anish Ramasekar <anish.ramasekar@gmail.com>
2022-08-15 13:30:20 -0700

    chore: update to go 1.19 (#960)
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>
    
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/Azure/secrets-store-csi-driver-provider-azure
 
-go 1.18
+go 1.19
 
 require (
 	github.com/Azure/azure-sdk-for-go v66.0.0+incompatible
--- pkg/provider/mock_provider/doc.go
+++ pkg/provider/mock_provider/doc.go
@@ -1,3 +1,4 @@
 // Run go generate to regenerate this mock.
+//
 //go:generate ../../../.tools/mockgen -destination provider_mock.go -package mock_provider -source ../provider.go
 package mock_provider //nolint
